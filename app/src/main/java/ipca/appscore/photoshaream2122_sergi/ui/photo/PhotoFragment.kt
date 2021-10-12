80931ed75ee36c0b91f68415c54ab2bac921f5c5
sergi <!Redevill12>
2021-10-12 17:41:33 +0100

    Update: - PhotoFragment
    Add Items:
    ...
    
    Notes:
    - take photo and using android camera adding to ImageView

--- app/src/main/java/ipca/appscore/photoshaream2122_sergi/ui/photo/PhotoFragment.kt
+++ app/src/main/java/ipca/appscore/photoshaream2122_sergi/ui/photo/PhotoFragment.kt
@@ -1,38 +1,79 @@
 package ipca.appscore.photoshaream2122_sergi.ui.photo
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import ipca.appscore.photoshaream2122_sergi.R
-import ipca.appscore.photoshaream2122_sergi.databinding.ActivityMainBinding
-import ipca.appscore.photoshaream2122_sergi.databinding.FragmentHomeBinding
 import ipca.appscore.photoshaream2122_sergi.databinding.FragmentPhotoBinding
+import ipca.appscore.photoshaream2122_sergi.models.Photo
+import java.io.ByteArrayOutputStream
 
 
 class PhotoFragment : Fragment() {
 
     private var _binding: FragmentPhotoBinding? = null
+    private val binding get() = _binding!!
 
-        private val binding get() = _binding!!
+    private var mbitmap : Bitmap? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inf
         // late the layout for this fragment
         _binding = FragmentPhotoBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        return _binding!!.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabTakePhoto.setOnClickListener {
+            val takePictureIntent = Intent (MediaStore.ACTION_IMAGE_CAPTURE)
+            //val intent  = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(takePictureIntent, CAMERA_PIC_REQUEST )
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK){
+            when(requestCode){
+                CAMERA_PIC_REQUEST -> {
+                    val bm : Bitmap? = data?.extras?.get("data") as? Bitmap
+                    bm?.let {
+                        binding.imageViewPhotoView.setImageBitmap(it)
+                        mbitmap = bm
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object{
+        const val CAMERA_PIC_REQUEST = 1001
+
     }
 
 }
